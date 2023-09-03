10e8aed18b5a5e81d131aafbcaed89931134e6cf
ki960213 <ki960213@gmail.com>
2023-09-03 15:34:19 +0900

    feat: sheath 모듈 추가

---
+++ sheath/src/androidTest/java/com/ki960213/sheath/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package com.ki960213.sheath
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.ki960213.sheath.test", appContext.packageName)
+    }
+}
