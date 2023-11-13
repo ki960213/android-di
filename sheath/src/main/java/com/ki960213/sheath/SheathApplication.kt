4f4c447b857f8ffac3e72c92939b56b1746103df
ki960213 <ki960213@gmail.com>
2023-11-13 20:21:35 +0900

    refactor(SheathContainer): SheathContainer가 Sheath 컴포넌트를 정렬 및 인스턴스화 하도록 변경

--- sheath/src/main/java/com/ki960213/sheath/SheathApplication.kt
+++ sheath/src/main/java/com/ki960213/sheath/SheathApplication.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import com.ki960213.sheath.component.ContextSheathComponent
 import com.ki960213.sheath.component.SheathComponent
 import com.ki960213.sheath.scanner.ComponentScanner
-import com.ki960213.sheath.sorter.sorted
-import kotlin.reflect.KType
 
 object SheathApplication {
 
@@ -16,17 +14,6 @@ object SheathApplication {
         val components: List<SheathComponent> =
             scanner.findAll() + ContextSheathComponent(context)
 
-        initContainer(components)
-    }
-
-    private fun initContainer(components: List<SheathComponent>) {
-        val container: Map<KType, SheathComponent> = components.sorted()
-            .fold(mutableMapOf()) { acc, component ->
-                component.instantiate(acc.values.toList())
-                acc[component.type] = component
-                acc
-            }
-
-        sheathContainer = SheathContainer(container)
+        sheathContainer = SheathContainer.from(components)
     }
 }
--- sheath/src/main/java/com/ki960213/sheath/SheathContainer.kt
+++ sheath/src/main/java/com/ki960213/sheath/SheathContainer.kt
@@ -1,6 +1,7 @@
 package com.ki960213.sheath
 
 import com.ki960213.sheath.component.SheathComponent
+import com.ki960213.sheath.sorter.sorted
 import kotlin.reflect.KType
 import kotlin.reflect.full.isSupertypeOf
 import kotlin.reflect.jvm.jvmErasure
@@ -11,5 +12,17 @@ class SheathContainer(private val components: Map<KType, SheathComponent>) {
         return components[type]
             ?: components.values.find { component -> type.isSupertypeOf(component.type) }
             ?: throw IllegalArgumentException("${type.jvmErasure.qualifiedName} 클래스가 컴포넌트로 등록되지 않았습니다.")
+    }
+
+    companion object {
+        fun from(components: List<SheathComponent>): SheathContainer {
+            val initializedComponents = components.sorted()
+                .fold(mutableMapOf<KType, SheathComponent>()) { acc, component ->
+                    component.instantiate(acc.values.toList())
+                    acc[component.type] = component
+                    acc
+                }
+            return SheathContainer(initializedComponents)
+        }
     }
 }
